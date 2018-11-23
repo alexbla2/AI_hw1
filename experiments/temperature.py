38d53fa1fb8af498bb3714c24f3c62f2d4cf3c0f
Daniel <danielamram@campus.technion.ac.il>
2018-11-23 17:04:50 +0200

    experiments/temperature.py

--- experiments/temperature.py
+++ experiments/temperature.py
@@ -1,15 +1,42 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
+
 X = np.array([400, 450, 900, 390, 550])
+vector_range = 100
 
 # TODO: Write the code as explained in the instructions
-raise NotImplemented()  # TODO: remove!
+# raise NotImplemented()  # TODO: remove!
 
-print(P)
 
-for i in range(len(X)):
-    plt.plot(T, P[:, i], label=str(X[i]))
+def probabilities_vector(X, T):
+
+    x_pow_t_dic = {}
+    x_pow_t_sum_dic = {}
+    for x in X:
+        for t in T:
+            x_pow_t = np.power(x, np.divide(-1, t))
+            x_pow_t_dic[(x, t)] = x_pow_t
+            if t not in x_pow_t_sum_dic:
+                x_pow_t_sum_dic[t] = x_pow_t
+            else:
+                x_pow_t_sum_dic[t] += x_pow_t
+
+    vectors_dic = {}
+    for x in X:
+        probability_vector = []
+        for t in T:
+            probability_vector.append(np.divide(x_pow_t_dic[(x, t)], x_pow_t_sum_dic[t]))
+        vectors_dic[x] = probability_vector
+    return vectors_dic
+
+
+T = np.linspace(0.01, 5, vector_range)
+p_vectors = probabilities_vector(X, T)
+
+for i, x in enumerate(X):
+    P = p_vectors[x]
+    plt.plot(T, P, label=str(x))
 
 plt.xlabel("T")
 plt.ylabel("P")
